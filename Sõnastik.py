0501a8d8f52c2c2763c7c2888e06d81b9ba18734
opilane <opilane@HP-CZC2349HTY>
2024-01-18 16:42:15 +0200

    Add project files.

---
+++ Sõnastik.py
@@ -0,0 +1,87 @@
+﻿
+def loe_failist(f):
+    with open(f, 'r', encoding='utf-8') as fail:
+        return [rida.strip() for rida in fail]
+
+def loo_tõlked(r_list, e_list):
+    return {r: e for r, e in zip(r_list, e_list)}
+
+def lisa_sõna(word, t_dict, new_trans):
+    t_dict.setdefault(word, new_trans)
+
+def tõlgi_sõna(word, t_dict):
+    return t_dict.get(word, "Перевод не найден")
+
+
+rus = loe_failist("rus.txt") # Загрузка списков слов
+est = loe_failist("est.txt")
+
+
+trans_dict = loo_tõlked(rus, est) # Создание словаря для перевода
+
+
+user_word = input("Введите слово для перевода: ")# Запрос пользователя
+
+
+if user_word in trans_dict:
+    translated_word = tõlgi_sõna(user_word, trans_dict)
+    print(f"Перевод слова '{user_word}': {translated_word}")   # Проверка, есть ли слово в словаре
+else:
+ 
+    user_translation = input("Перевод для '{user_word}' не найден. Введите перевод: ")      # Запрос перевода и добавление в словарь
+    lisa_sõna(user_word, trans_dict, user_translation)
+    print("Слово '{user_word}' добавлено в словарь с переводом '{user_translation}'")
+
+
+print(trans_dict)   # Печать обновленного словаря
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#def loe_failist(f):
+#    with open(f, 'r', encoding='utf-8') as fail:
+#        return [rida.strip() for rida in fail]
+
+#def loo(rus_list, est_list):
+#    return {rus: est for rus, est in zip(rus_list, est_list)}
+
+#def translate(word, trans_dict):
+#    return trans_dict.get(word, "Перевод не найден")
+
+
+#rus = loe_failist("rus.txt")
+#est = loe_failist("est.txt")
+
+
+#trans_dict = loo(rus, est)
+
+
+#word_to_translate = "привет"
+#translated_word = translate(word_to_translate, trans_dict)
+#print(f"Перевод слова '{word_to_translate}': {translated_word}")
+
---
+++ module1.py
@@ -0,0 +1,40 @@
+﻿def loe_failist(fail:str)->list:
+   jarjend=[] 
+   raagi=""
+   f=open(fail,'r',encoding="utf-8")
+   for rida in f:
+        jarjend.append(rida.strip()) # В кавычках можно укказывать знак разделения
+        raagi+=rida
+   f.close()
+   return jarjend,raagi
+
+
+def Kirjuta_failisse(fail:str,jarjend:list):
+    f=open(fail,'w',encoding="utf-8")
+    for item in jarjend:
+        f.write(item+'\n')      
+    f.close()
+test_to_speech=""#---------------
+paevad, test_to_speech=loe_failist("Paevad.txt")#------------
+print(paevad)
+
+list_=[]
+for i in range(5):
+    nimi=input(str(i+1)+".Nimi ")
+    list_.append(nimi)
+Kirjuta_failisse("Nimed.txt",list_)
+
+with open('Nimed.txt', 'r') as f:
+    print(f.read())
+    
+from os import *
+if path.isfile("Nimi.txt"):   #path.isdir(kaust)
+    remove("Nimed.txt") #faili kustutamine, kui ta on olemas
+    from gtts import gTTS
+
+from gtts import gTTS
+def Heli(text:str,keel:str):
+    obj=gTTS(text=text,lang=keel,slow=False).save("heli.mp3")
+    system("heli.mp3")
+
+Heli(test_to_speech,"et")
